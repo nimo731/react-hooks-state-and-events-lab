261d842093cc3ba5e08e5bf507e86440bb2d9f2b
nimo731 <patience.karanja@student.moringaschool.com>
2025-04-17 10:40:08 +0300

    atate and event

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  function handleAddToCartClick() {
+    setIsInCart((prevState) => !prevState);
+  }
+
+  const liClass = isInCart ? "in-cart" : "";
+  const buttonText = isInCart ? "Remove From Cart" : "Add to Cart";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
+      <button className="add" onClick={handleAddToCartClick}>
+        {buttonText}
+      </button>
+      </li>
   );
-}
+} 
 
 export default Item;
---
+++ src/components/items.js
@@ -0,0 +1,9 @@
+const items = [
+    { id: 1, name: "Apple", category: "Produce" },
+    { id: 2, name: "Milk", category: "Dairy" },
+    { id: 3, name: "Cake", category: "Dessert" },
+    { id: 4, name: "Carrots", category: "Produce" },
+  ];
+  
+  export default items;
+  
